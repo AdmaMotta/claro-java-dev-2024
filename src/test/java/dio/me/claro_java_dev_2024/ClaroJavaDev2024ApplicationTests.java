99c9f2c878bd30f70d7a2117e61fd6eff22d64aa
AdmaMotta <admafui@hotmail.com>
2024-09-11 16:09:59 -0300

    Initial commit

---
+++ src/test/java/dio/me/claro_java_dev_2024/ClaroJavaDev2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.claro_java_dev_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroJavaDev2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
